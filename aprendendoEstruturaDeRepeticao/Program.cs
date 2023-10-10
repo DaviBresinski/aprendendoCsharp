c88e3ac682b28712f3f9ca82f3faadbc66240d95
DaviBresinski <147429095+DaviBresinski@users.noreply.github.com>
2023-10-10 16:40:46 -0300

    aprendendo condicoes e repeticoes

---
+++ aprendendoEstruturaDeRepeticao/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Schema;
+
+
+
+
+
+
+
+
+
+
+
+
+// var n1=10;
+// var n2=8;
+// var n3=3;
+// var total=(n1+n2+n3)/3;
+//  Console.WriteLine("total: "+total);
+
+// if(total>=6){
+//     Console.WriteLine("passou");    
+// }
+// else if(total<6 && total>=4)
+// {
+//      Console.WriteLine("recuperacao");
+// }else
+// {
+//     Console.WriteLine("reprovou");
+// };
+
+
+// var n1=15;
+// var n2=10;
+// var n3=20;
+// var total=(n1+n2+n3)/3;
+// Console.WriteLine("total: "+total);
+
+// int contador = 0 ;
+// for (int i = 1; i < 30; i++)
+// {  
+//     contador++;
+//     if (i == 25)
+//     Console.WriteLine("Hello, World!: "+contador);  
+// }
+// Console.WriteLine("quantidade: "+contador);
+
+
