59a3c88d56b00864842f84309f60f1151464f612
DaviBresinski <147429095+DaviBresinski@users.noreply.github.com>
2023-10-19 18:15:07 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+{
+    string[] nomes = { "matheus", "davi", "valentina", "andrea" };
+
+
+    Console.WriteLine("escreva um nome");
+
+    {
+        if (Console.ReadLine() == "matheus" || Console.ReadLine() == "davi")
+        {
+            Console.WriteLine("este nome esta listado e o sujeito e homem");
+        }
+
+        else if (Console.ReadLine() == "andrea" || Console.ReadLine() == "valentina")
+        {
+
+            Console.WriteLine("este nome nao esta listado e o sujeito e mulher");
+        }
+        else
+        {
+
+            Console.WriteLine("o nome nao esta listado");
+        }
+    }
+}
+
+
+
+
+
+            //int matheus;
+            //int davi;
+            //int andrea;
+            //int valentina;      
