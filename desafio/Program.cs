5b9796bcb12717d1c3bd2c55117be2daad6d4391
Davi <adechecon@gmail.com>
2023-10-26 16:58:52 -0300

    terminei o quinto desafio

---
+++ desafio/Program.cs
@@ -0,0 +1,66 @@
+﻿//5
+Console.WriteLine("Digite o seu peso");
+
+var seu = Console.ReadLine();
+
+Console.WriteLine("digite o peso de mais uma pessoa");
+
+var pessoa1 = Console.ReadLine();
+
+Console.WriteLine("digite o peso de outra pessoa");
+
+var pessoa2 = Console.ReadLine();
+
+Console.WriteLine("seu: "+seu);
+
+Console.WriteLine("pessoa1: "+pessoa1);
+
+Console.WriteLine("pessoa2: "+pessoa2);
+
+// Console.ReadLine();
+
+// Console.WriteLine(peso);
+
+//4
+//Console.WriteLine("Digite sua altura");
+
+//string altura = Console.ReadLine();
+
+//Console.WriteLine("altura: "+altura);
+
+//3
+// Console.WriteLine("escolha um numero ");
+
+// var numero = Console.ReadLine();
+// Console.WriteLine("escolha a quantidade de casas decimais");
+// var escolha = Console.ReadLine();
+
+// var tete =Convert.ToDouble(numero) ;
+
+// switch (escolha)
+// {
+//     case "2":
+//     Console.WriteLine(tete.ToString("N2"));
+//     break;
+//     case "5":
+//     Console.WriteLine(tete.ToString("N5"));
+//     break;
+    
+//     default:
+//     Console.WriteLine(tete.ToString("N1"));
+//     break;
+// }
+
+// Console.WriteLine(numero.ToString("N1"));
+
+// Console.WriteLine(numero.ToString("N2"));
+
+// Console.WriteLine(numero.ToString("N5"));
+
+
+//2
+//Console.WriteLine("Davi \nBresinski");
+
+
+//1
+//Console.WriteLine("Cachoeiro de itapemirim")
